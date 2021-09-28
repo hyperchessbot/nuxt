612c9c10e9686d3156a46a6d78bf6e41ca9777c6
hyperchessbot <hyperchessbot@gmail.com>
2021-09-28 21:09:04 +0000

    Fetch index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <Tutorial/>
+<div>
+    <li v-for="item in items" :key="item.id">
+    {{ item.name }}
+  </li>
+</div>
 </template>
 
 <script>
-export default {}
+export default {  
+  async asyncData() {
+      console.log("fetching")
+      const response = await fetch(`https://raw.githubusercontent.com/hyperchessbot/nuxt/main/app/recipes/index`)
+      const content = await response.text()
+      console.log("fetched", content)
+      const items = content.split("------").map(item => {
+            const fields = item.split("---")
+            return {
+              id: fields[0],
+              name: fields[1],
+              ingredients: fields[2]
+            }
+          })
+      return { items }
+    },
+}
 </script>
