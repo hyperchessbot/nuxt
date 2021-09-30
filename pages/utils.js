a2c22c2628b05ce132aceeb73cd7d170c133adbb
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2021-09-30 07:53:17 +0200

    Index in toml

--- pages/utils.js
+++ pages/utils.js
@@ -1,3 +1,5 @@
+import toml from "toml";
+
 export default {
   config: {
     RECIPES_GIT_BASE:
@@ -6,15 +8,7 @@ export default {
   },
   methods: {
     index2blob(content) {
-      const items = content.split("------").map((item) => {
-        const fields = item.split("---").map((item) => item.trim());
-        return {
-          id: fields[0],
-          name: fields[1],
-          ingredients: fields[2],
-          thumbnail: fields[3]
-        };
-      });
+      const items = toml.parse(content);
 
       return items;
     }
