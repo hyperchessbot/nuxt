73c592da08703f41a654d4f15ce3db947af621b6
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2021-09-30 09:10:32 +0200

    Chess

---
+++ pages/chess.vue
@@ -0,0 +1,14 @@
+<template>
+  <div><chessboard></chessboard>></div>
+</template>
+
+<script>
+import { chessboard } from "vue-chessboard";
+import "vue-chessboard/dist/vue-chessboard.css";
+
+export default {
+  components: {
+    chessboard,
+  },
+};
+</script>
