585e4ed16f6d2070ebf66fd7df25c40740ecd43c
hyperchessbot <hyperchessbot@gmail.com>
2021-10-01 11:59:49 +0000

    Reset

--- chess/pages/index.vue
+++ chess/pages/index.vue
@@ -3,6 +3,7 @@
   <div class="controls">
     <input type="text" id="moveinput" v-on:keyup.enter="makemove" />
     <button v-on:click="makemove">Make move</button>
+    <button v-on:click="reset">Reset</button>
   </div>
   <chessboard :fen="currentFen" />
 </div>
@@ -17,12 +18,23 @@ function makemove(self){
   const moveinpute = document.getElementById('moveinput')
   const san = moveinpute.value
   moveinpute.value = ""
+  try{    
   const pos = self.pos
   console.log("making move", san, "for", pos.toString())
   pos.playSan(san)
   self.currentFen = pos.reportFen()
   console.log("done", pos.toString())
   self.pos = pos
+  } catch(err){
+    window.alert(`Invalid move ${san} !`)
+  }
+}
+
+function reset(self){
+  const pos = self.pos
+  pos.setVariant(pos.rules)
+  self.pos = pos
+  self.currentFen = pos.reportFen()
 }
 
 export default {
@@ -40,6 +52,9 @@ export default {
   methods:{
     makemove(){      
       makemove(this)
+    },
+    reset(){
+      reset(this)
     }
   }
 }
